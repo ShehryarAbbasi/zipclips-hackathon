edaff9caf084d7900c7dcb3cea316c170e76e1ac
Patrick Ford <patrickford@msn.com>
2014-02-10 16:07:03 -0800

    Added: user icon and username display, datetime stamps to video clip data, geolocation map.  Need to complete server implementation to enable and test map.

--- js/zipclips.js
+++ js/zipclips.js
@@ -1,11 +1,48 @@
 // Create video elements for each entry in clipData
 
+var now = new Date();
+
 for (var i = 0; i < clipData.length; i++) {
-  clipTag = '<video class = "mainvideo" controls="controls" width="300" height="225" name="' + clipData[i].title +
+  clipTag = '<video class = "videoclip" controls="controls" width="300" height="225" name="' + clipData[i].title +
             '" src="' + clipData[i].url +'"></video>';
   $("#videos").append(clipTag);
 }
 
-$(".mainvideo").dblclick(function() {
+$(".videoclip").dblclick(function() {
   alert("This video was double clicked");
 });
+
+$(".videoclip").hover(
+  function() {
+    $(this).append($("<span> Video Clip Data </span>"));
+  }, function() {
+    $(this).find("span:last").remove(); 
+  }
+);
+
+var x = document.getElementById("mapholder");
+
+var getLocation = function() {
+  if (Modernizr.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    x.innerHTML = "Geolocation is not supported.";
+  }
+};
+
+var showPosition = function(position) {
+  console.log("Running showPosition");
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+  var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    + latlon + "&zoom=14&size=400x300&sensor=false";
+    $("mapholder").innerHTML = "<img src='" + img_url + "'>";
+
+  console.log(img_url);
+};
+
+var showError = function() {
+  console.log('Geolocation.getCurrentPosition failed.')
+};
+
+getLocation();
