ff46aa77b505aa3df0162c90457a478e0eae119b
Patrick Ford <patrickford@msn.com>
2014-02-11 11:05:53 -0800

    Basic setup of Angular.js controllers.

---
+++ server/public/js/controllers/events.js
@@ -0,0 +1,10 @@
+function EventCtrl ($scope) {
+  $scope.events = {
+    "eventID": 100,
+    "eventName": "Hack Reactor",
+    "location" : {
+      "latitude" : 37.7836953,
+      "longitude" : -122.4091845
+    }
+  }
+}
