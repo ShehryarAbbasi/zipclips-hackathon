00d324b9831bf875a6db0924e96a7a5f3d377cd6
Patrick Ford <patrickford@msn.com>
2014-02-11 15:02:29 -0800

    Clip titles edited and css changed for title list display.

--- server/public/js/controllers/clips.js
+++ server/public/js/controllers/clips.js
@@ -2,81 +2,89 @@ function ClipsCtrl ($scope) {
   $scope.clips = {
     "100" : {
       "url": "./videos/video1.mov",
-      "title": "Day at HR - 1",
+      "title": "The Hackathon Begins!",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 15:31:20 GMT-0800 (PST)",
       "eventID": 100
     },
     "101" : {
       "url": "./videos/video2.mov",
-      "title": "Day at HR - 2",
+      "title": "Lecture Room at Hack Reactor",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 15:27:35 GMT-0800 (PST)",
       "eventID": 100
     },
     "102" : {
       "url": "./videos/video3.mov",
-      "title": "Day at HR - 3",
+      "title": "Workstations at Hack Reactor",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 15:24:41 GMT-0800 (PST)",
       "eventID": 100
     },
     "103": {
       "url": "./videos/IMG_0100.mov",
-      "title": "Day at HR - 4",
+      "title": "Rainy Day on Market St.",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 13:25:50 GMT-0800 (PST)",
       "eventID": 100
     },
     "104": {
       "url": "./videos/IMG_0101.mov",
-      "title": "Day at HR - 5",
+      "title": "Feet on Market St.",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 13:10:05 GMT-0800 (PST)",
       "eventID": 100
     },
     "105": {
       "url": "./videos/IMG_0102.mov",
-      "title": "Day at HR - 6",
+      "title": "Watch Out for Busses",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 14:27:10 GMT-0800 (PST)",
       "eventID": 100
     },
     "106": {
       "url": "./videos/IMG_0103.mov",
-      "title": "Day at HR - 7",
+      "title": "Juan Does His Magic",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 14:16:15 GMT-0800 (PST)",
       "eventID": 100
     },
     "107": {
       "url": "./videos/IMG_0104.mov",
-      "title": "Day at HR - 8",
+      "title": "Brian Makes a Game",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 14:23:16 GMT-0800 (PST)",
       "eventID": 100
     },
     "108": {
       "url": "./videos/IMG_0105.mov",
-      "title": "Day at HR - 9",
+      "title": "Stand Up Workstations",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 14:25:17 GMT-0800 (PST)",
       "eventID": 100
     },
     "109": {
       "url": "./videos/IMG_0106.mov",
-      "title": "Day at HR - 10",
+      "title": "Brendon Hacks Away",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 14:30:20 GMT-0800 (PST)",
       "eventID": 100
     },
     "110": {
       "url": "./videos/IMG_0107.mov",
-      "title": "Day at HR - 11",
+      "title": "Davy and Autumn",
       "author": "patrickford",
       "createdAt": "Mon Feb 10 2014 14:09:21 GMT-0800 (PST)",
       "eventID": 100
     }
   }
+
+  function showData(){
+    alert("Video has been double clicked");
+  };
+   
 };
+
 console.log('Clips Control Loaded');
+
+
